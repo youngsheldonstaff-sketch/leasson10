48894f44f272aaf27987d3e6ac01a284188ffc83
youngsheldonstaff-sketch <youngsheldonstaff@gmail.com>
2025-11-23 10:49:46 +0300

    homework (power calculator)

---
+++ homework2.py
@@ -0,0 +1,9 @@
+
+
+base_number = int(input("Enter the base number: "))
+exponent = int(input("Enter the power (n) you want to raise it to: "))
+
+result = base_number ** exponent
+
+
+print(f"\n{base_number} raised to the power of {exponent} is: {result}")
